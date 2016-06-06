c079a1d913c10de8a0562dcc5127d88e09682bb0
jcdejong <jcdejong@allict.nl>
2016-06-06 11:14:03 +0200

    quick horizontal/vertical output

--- jcdejong/challenge.php
+++ jcdejong/challenge.php
@@ -24,8 +24,44 @@ $challenge->initWordDb();
 $output = 'Here is the rebus to solve: ';
 echo PHP_EOL . $output . PHP_EOL . str_repeat('-', strlen($output)) . PHP_EOL;
 
+// @todo add images using a google/flickr search, prettify, test, etc ;)
 $words = explode(' ', $string);
+$output = [];
 foreach ($words as $word) {
     $rebus = new Challenge\Rebus($word, $entityManager);
+    ob_start();
     $rebus->dump();
+    $output[] = explode(PHP_EOL, ob_get_clean());
+}
+
+//vertical dump rebus
+if (isset($argv[2]) && strpos($argv[2], 'vertical') !== false) {
+    foreach ($output as $rebus) {
+        echo implode(PHP_EOL, $rebus);
+        echo '----------------------------' . PHP_EOL;
+    }
+} else {
+    // horizontal dump :)
+    $colSize = [];
+    echo '|';
+    foreach ($output as $key => $word) {
+        if (isset($word[0])) {
+            echo str_pad($word[0], strlen($word[0]) + 5, ' ', STR_PAD_BOTH) . '|';
+
+            $colSize[$key] = strlen($word[0]) + 5;
+        }
+    }
+    echo PHP_EOL . '|';
+    foreach ($output as $key => $word) {
+        if (isset($word[1])) {
+            echo str_pad($word[1], $colSize[$key], ' ', STR_PAD_BOTH) . '|';
+        }
+    }
+    echo PHP_EOL . '|';
+    foreach ($output as $key => $word) {
+        if (isset($word[2])) {
+            echo str_pad($word[2], $colSize[$key], ' ', STR_PAD_BOTH) . '|';
+        }
+    }
+    echo PHP_EOL;
 }
